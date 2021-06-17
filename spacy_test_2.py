21ccafeaac4d0ac7737b101e8aa2f039949b771f
SAN <12975157+thegitparticle@users.noreply.github.com>
2021-06-17 15:44:28 +0530

    test_2 file added

---
+++ spacy_test_2.py
@@ -0,0 +1,50 @@
+import spacy
+import streamlit as st
+
+# nlp = spacy.load('en_core_web_trf')
+nlp = spacy.load("en_core_web_lg")
+
+
+def keyword(message):
+
+    doc_here = nlp(message)
+
+    x = ""
+
+    for token in doc_here:
+        if token.dep_ == "nsubj":
+            # print(token.text + ' ' + 'nsubj')
+            if (
+                token.text == "I"
+                or token.text == "i"
+                or token.text == "You"
+                or token.text == "you"
+                or token.text == "YOU"
+                or token.pos_ == "PRON"
+            ):
+                # print(token.text + ' ' + 'i or you')
+                for token in doc_here:
+                    if token.pos_ == "NOUN":
+                        x = token.text
+                        break
+            else:
+                x = token.text
+                break
+
+    if len(x) == 0:
+        for token in doc_here:
+            if token.pos_ == "ADJ":
+                x = token.text
+                break
+
+    if len(x) == 0:
+        x = message
+
+    return x
+
+
+text = st.text_area("enter your review here")
+
+if st.button("show"):
+    response = keyword(text)
+    "full review: ", response
